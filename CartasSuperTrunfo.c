85bc989459bb8457b6bfa048848a52866e4c89bf
ogumelo <augusto.s.melo@icloud.com>
2025-03-28 22:21:46 +0000

    Carta1 Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
-int main(){
+int main() {   
 printf("Desafio Super Trunfo!\n");
+
+
+    char estado = 'A';
+    char codigo (A01) ;
+    char cidade = ("Rio Branco");
+    int populacao = 800.000;
+    float area = 250.000;
+    float pib = 950.000;
+    int pontosturisticos = 20;
+
+
+ 
+    printf("estado: %s\n" ,'letra');
+    printf("codigo: %f\n", codigo);
+    printf("cidade: %c\n", 'nome' );
+    printf("populacao: %.2f\n", "populacaototal");
+    printf("area: %.2f\n", "areatotal");
+    printf("pib: %.2f\n" , "pibtotal");
+    printf("pontosturisticos: %d\n" , "quantidade");
+ 
+    
 }
